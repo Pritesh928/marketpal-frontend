e0f09273c943006e0d2413c0f188051e9897e0ad
Pritesh <pptsingh123@gmail.com>
2026-07-13 03:52:47 +0530

    feat : api integration

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+const BASE_URL = process.env.API_URL || "https://marketpal-backend-ol74.onrender.com";
 
 const api = axios.create({
   baseURL: BASE_URL,
