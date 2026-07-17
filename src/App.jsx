39795021b82243af5c96bbf1a44d0e2dfce354bd
Pritesh <pptsingh123@gmail.com>
2026-07-18 00:44:19 +0530

    fix : sell page routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-// import ProtectedRoute from "./components/ProtectedRoute";
 
+import ProtectedRoute from "./components/ProtectedRoute";
 import UnderDevelopment from "./pages/UnderDevelopment"; 
 
 // this all pages are under development will be available soon.
@@ -10,7 +10,7 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-// import SellPage from "./pages/SellPage";
+import Sell from "./pages/Sell";
 // import MyProductsPage from "./pages/MyProductsPage";
 // import ProductDetailPage from "./pages/ProductDetailPage";
 
@@ -30,8 +30,10 @@ function AppRoutes() {
       <Route path="/home" element={<Home />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/sell" element={
+      <ProtectedRoute> <Sell /> </ProtectedRoute>
+      } />
       {/* <Route path="/product/:id" element={<ProductDetailPage />} /> */}
-      {/* <Route path="/sell" element={<ProtectedRoute><SellPage /></ProtectedRoute>} /> */}
       {/* <Route path="/products" element={<ProtectedRoute><MyProductsPage /></ProtectedRoute>} /> */}
 
       <Route path="*" element={<Navigate to="/" replace />} />
