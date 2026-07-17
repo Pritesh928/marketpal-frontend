95555446d1863bedbb0173ce2d48bd5163c1626c
Pritesh <pptsingh123@gmail.com>
2026-07-18 01:51:08 +0530

    feat : reproducing error for getting detail on the error

--- src/service/api.js
+++ src/service/api.js
@@ -26,7 +26,7 @@ api.interceptors.response.use(
     if (error.response?.status === 401 || error.response?.status === 403) {
       localStorage.removeItem("jwtToken");
       localStorage.removeItem("user");
-      window.location.href = "/login";
+      // window.location.href = "/login";
     }
     return Promise.reject(error);
   }
